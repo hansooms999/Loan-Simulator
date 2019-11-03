ad1f69d62ca2e9711fb5b1eb43b55db9eef3a6db
Alex Hoet <hoet.9@osu.edu>
2019-11-03 10:50:08 -0500

    Attempted to add a remove button to the index page.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var maxField = 10; //Input fields increment limitation
   var addButton = $(".add-button"); //Add button selector
+  var removeButton = $(".remove-button"); //Remove button selector
   var wrapper = $(".field-wrapper"); //Input field wrapper
   var x = 1; //Initial field counter is 1
 
@@ -11,23 +12,51 @@ $(document).ready(function() {
       x++; //Increment field counter
       var fieldHTML = `
           <div class="loan-input">
-        <h5>Loan ${x}:</h5>
-        <div class="loan-balance">
-            <label for="loan-balance">Loan Balance:</label>
-            <input type="number" class="principal" name="principal" min="1" required />
-        </div>
-        <div class="interest-rate">
-            <label for="interest-rate">Interest Rate:</label>
-            <input type="number" class="interest" name="interestRate" max="100" min="1" required />
-        </div>
-        <div class="subsidized-option">
-            <label for="subsidize-option">Subsidized:</label>
-            <input type="radio" class="subsidized" name="subsidize${x}" value="unsubsidized" required >Unsubsidized</input>
-            <input type="radio" class="subsidized" name="subsidize${x}" value="subsidized">Subsidized</input>
-            <label class="reveal-active" for="graduation-date">Graduation Month:</label>
-            <input class="graduation-date reveal-active" type="month" name="graduation-month" value="2020-05">
-            </div>`; //New input field html
+            <table>
+              <thead>
+                <tr>
+                  <th colspan="2"><h5>Loan ${x}:</h5></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <div class="loan-balance">
+                      <label for="loan-balance">Loan Balance:</label>
+                      <input type="number" class="principal" name="principal" min="1" required />
+                    </div>
+                    <div class="interest-rate">
+                      <label for="interest-rate">Interest Rate:</label>
+                      <input type="number" class="interest" name="interestRate" max="100" min="1" required />
+                    </div>
+                    <div class="subsidized-option">
+                      <label for="subsidize-option">Subsidized:</label>
+                      <input type="radio" class="subsidized" name="subsidize${x}" value="unsubsidized" required >Unsubsidized</input>
+                      <input type="radio" class="subsidized" name="subsidize${x}" value="subsidized">Subsidized</input>
+                      <label class="reveal-active" for="graduation-date">Graduation Month:</label>
+                      <input class="graduation-date reveal-active" type="month" name="graduation-month" value="2020-05">
+                    </div>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-primary btn-circle btn-sm remove-button">X</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        `; //New input field html
+        
+        
       $(wrapper).append(fieldHTML); //Add field html
+
+      let rb = $(".remove-button:last-child")[0];
+      rb.onclick = function(){
+        if (x > 1) {
+          let lastLoan = rb.parentNode.parentNode.parentNode.parentNode.parentNode;
+          lastLoan.remove();
+          x--;
+        }
+      };
     }
   });
 
