158e2f00a48d2e66f9ad2a2a627141c02eddb80b
arlunn <ezehat@gmail.com>
2019-11-02 20:54:24 -0400

    working loan page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,30 +1,3 @@
-function sendData() {
-  var principals = document.getElementsByClassName("principal");
-  var interests = document.getElementsByClassName("interest");
-  var gradDate = document.getElementsByClassName("graduation-date");
-
-  var arr = [];
-  for (var i = 0; i < principals.length; i++) {
-    var subsidizedVal = $(`input[name="subsidized${i + 1}"]:checked`).val();
-    var sub;
-    var date;
-    if (gradDate[i] == null) {
-      date = "000000";
-    } else {
-      date =
-        gradDate[i].value.substring(5, 7) + gradDate[i].value.substring(0, 4);
-    }
-    if (subsidizedVal == "subsidized") {
-      sub = true;
-    } else {
-      sub = false;
-    }
-    arr.push([principals[i].value, interests[i].value, sub, date]);
-  }
-
-  localStorage.setItem("loansArray", arr);
-}
-
 $(document).ready(function() {
   var maxField = 10; //Input fields increment limitation
   var addButton = $(".add-button"); //Add button selector
@@ -41,17 +14,17 @@ $(document).ready(function() {
         <h5>Loan ${x}:</h5>
         <div class="loan-balance">
             <label for="loan-balance">Loan Balance:</label>
-            <input type="number" class="principal" name="principal" required />
+            <input type="number" class="principal" name="principal" min="1" required />
         </div>
         <div class="interest-rate">
             <label for="interest-rate">Interest Rate:</label>
-            <input type="number" class="interest" name="interestRate" required />
+            <input type="number" class="interest" name="interestRate" max="100" min="1" required />
         </div>
         <div class="subsidized-option">
             <label for="subsidize-option">Subsidized:</label>
             <input type="radio" class="subsidized" name="subsidize${x}" value="unsubsidized" required >Unsubsidized</input>
             <input type="radio" class="subsidized" name="subsidize${x}" value="subsidized">Subsidized</input>
-            <label class="graduation-date reveal-active" for="graduation-date">Graduation Month:</label>
+            <label class="reveal-active" for="graduation-date">Graduation Month:</label>
             <input class="graduation-date reveal-active" type="month" name="graduation-month" value="2020-05">
             </div>`; //New input field html
       $(wrapper).append(fieldHTML); //Add field html
@@ -65,5 +38,37 @@ $(document).ready(function() {
       .parent("div")
       .remove(); //Remove field html
     x--; //Decrement field counter
+  });
+
+  $("#form").submit(function() {
+    var principals = document.getElementsByClassName("principal");
+    var interests = document.getElementsByClassName("interest");
+    var gradDate = document.getElementsByClassName("graduation-date");
+
+    var arr = new Array(principals.length);
+    for (var i = 0; i < principals.length; i++) {
+      var subsidizedVal = $(`input[name="subsidize${i + 1}"]:checked`).val();
+      var sub;
+      var date;
+      if (gradDate[i] == null) {
+        date = "000000";
+      } else {
+        date =
+          gradDate[i].value.substring(5, 7) + gradDate[i].value.substring(0, 4);
+      }
+      if (subsidizedVal == "subsidized") {
+        sub = true;
+      } else {
+        sub = false;
+      }
+      arr[i] = new Array(4);
+      arr[i][0] = principals[i].value;
+      arr[i][1] = interests[i].value;
+      arr[i][2] = sub;
+      arr[i][3] = date;
+      //arr.push([principals[i].value, interests[i].value, sub, date]);
+    }
+
+    localStorage.setItem("loansArray", JSON.stringify(arr));
   });
 });
